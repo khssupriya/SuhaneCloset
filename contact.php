86a215a5ce4176bae91a728b3f8586d2e8aaaffb
Miss_Supriya <52968975+khssupriya@users.noreply.github.com>
2020-04-22 18:03:53 +0530

    Update contact.php

--- contact.php
+++ contact.php
@@ -1,3 +1,6 @@
+<?php
+        session_start();
+?>
 <!DOCTYPE html>
 <html>
 <head>
@@ -17,7 +20,7 @@
 
 		<!-- -------- NavMenu Begins --------->
 
-	<div id = "NavMenu">
+		<div id = "NavMenu">
 		<ul>
 			<span id = "title">
 				<li><a href="index.php">Suhane's Closet</a></li>				
@@ -94,7 +97,30 @@
 	</footer>
 
 	<!-- -------- Footer ends --------->
+        
+        
+        <!-----------SCRIPT FOR MODAL --------->
 
+	<script type="text/javascript">
+		// Get the modal
+		var modal = document.getElementById("myModal");
+
+		// Get the button that opens the modal
+		//var btn = document.getElementById("orderlinkbutn");
+
+		// Get the <span> element that closes the modal
+		var span = document.getElementsByClassName("close")[0];
+
+		// When the user clicks the button, open the modal 
+		cart.onclick = function() {
+		  modal.style.display = "block";
+		}
+
+		// When the user clicks on <span> (x), close the modal
+		span.onclick = function() {
+		  modal.style.display = "none";
+		}
+	</script>
 
 
 </body>
